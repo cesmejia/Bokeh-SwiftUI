fbaa1c2009c68d6464e77189bdad207e3869aab4
Cesar Mejia V <cesmejia8@gmail.com>
2022-05-18 22:04:24 -0500

    Initial Commit

---
+++ BokehSwiftUI/BokehSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BokehSwiftUIApp.swift
+//  BokehSwiftUI
+//
+//  Created by Cesar Mejia Valero on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BokehSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
